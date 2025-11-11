a65a093876f4880667c465302bede42fd116497a
GustavoPlay002 <gustavofoeschn@gmail.com>
2025-11-11 17:53:26 -0300

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>EducaWeb</h1>
+      <p>Bem-vindo ao seu portal de cursos digitais!</p>
+    </div>
+  );
+}
